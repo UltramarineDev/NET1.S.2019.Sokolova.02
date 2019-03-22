1eda648e798606f161dc8512847a5259608492c1
Marina Sokolova <sokolova.m.e9@gmail.com>
2019-03-22 19:52:54 +0300

    add solution for task 2

---
+++ MaxElementSearch.Tests/SearchAlgoritmTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MaxElementSearch.Tests
+{
+    public class SearchAlgoritmTests
+    {
+        [TestCase(new int[] { 0, int.MaxValue, 80, 890000, 12, 0, 64 }, ExpectedResult = int.MaxValue)]
+        [TestCase(new int[] { 65, 3, 1, 7, 432, 7563, 55, -431, -78, 0 }, ExpectedResult = 7563)]
+        [TestCase(new int[] { -1000, -567, int.MinValue, -98, -6, -67, -67 }, ExpectedResult = -6)]
+        [TestCase(new int[] { 9, 9, 9, 9, 9, 9, 9 }, ExpectedResult = 9)]
+        public int FindMaxElementTests(int[] array)
+            => SearchAlgorithm.FindMaxElement(array);
+
+        [Test]
+        public void FindMaxElement_EmptyArray_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => SearchAlgorithm.FindMaxElement(new int[] { }), message: "Source array can not be empty");
+        }
+
+        [Test]
+        public void FindMaxElement_ArrayIsNull_ThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => SearchAlgorithm.FindMaxElement(null), message: "Source array can not be null");
+        }
+    }
+}
---
+++ MaxElementSearch/SearchAlgorithm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxElementSearch
+{
+    /// <summary>
+    /// Search Algorithm class
+    /// </summary>
+    public static class SearchAlgorithm
+    {
+        /// <summary>
+        /// Method finds maximum element in array of integers using recursive search algorithm.
+        /// </summary>
+        /// <param name="array">source array of integers</param>
+        /// <returns>maximum value</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the input array is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when the input array is epmty.</exception>
+        public static int FindMaxElement(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(nameof(array));
+            }
+
+            int maxElement = array[array.Length - 1];
+            int index = array.Length - 1;      
+            return FindMaxElementHelp(maxElement, array, index);
+        }
+
+        private static int FindMaxElementHelp(int maxElement, int[] array, int index)
+        {
+            if (index < 1)
+            {
+                return maxElement;
+            }
+
+            if (maxElement < array[index - 1])
+            {
+                maxElement = array[index - 1];  
+            }    
+            
+            return FindMaxElementHelp(maxElement, array, index - 1);         
+        }
+    }
+}
